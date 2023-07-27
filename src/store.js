9b9ddce2fec731652940cc23e94e40e9ac50bef8
carlosDevf <carlos@devf.mx>
2023-07-26 21:27:43 -0600

    implentacion asincrona

--- src/store.js
+++ src/store.js
@@ -1,12 +1,26 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { useDispatch, useSelector } from 'react-redux'
-// funciones asincronas
-// pending
-// fullfilled
+
+// persisteDAta
 
 const initialState = {
     cart: []
 }
+
+const addToCartAsync = createAsyncThunk('store-redux/addToCartAsync', async (item) => {
+	console.log(item)
+	// otros metodos que tenemos para ejecutar funciones asincronas
+	// crear una funcion asincrona	
+	// console.log('setTimeOut', item)
+	// return item
+	// peticion asincrona con fetch
+	return fetch('https://fakestoreapi.com/products/1')
+	.then(res=>res.json())
+	.then(json=> {
+		console.log(json)
+		return json
+	})	
+})
 
 // todas las acciones que necesitamos
 const storeSlice = createSlice({
@@ -14,6 +28,39 @@ const storeSlice = createSlice({
 	initialState,
 		reducers: {
 			addToCart (state, action) {
+				// 
+				const currentProduct = action.payload
+				// filter [] y find {}
+				// 
+				const isExist = state.cart.find((product) => product.id === currentProduct.id)
+				// si es falso retorna un undefined				
+
+				if(isExist) {
+					// en caso de no hacer lo de la linea 27 esto pasaria
+					// undefined + numero = NaN
+					isExist.cantidad += 1
+					// isExist.cantidad = isExist.cantidad + 1
+				} else {
+					// importante añadir la propiedad de cantidad al objeto que voy a añadir al arreglo de carrito
+					currentProduct.cantidad = 1					
+					state.cart.push(currentProduct)
+				}				
+			},
+			removeToCart (state, action) {
+				const currentProuct = action.payload
+				// buscar el producto en el arreglo de cart
+				// si existe => lo eliminamos => creamos un nuevo arreglo sin ese elemento
+				// indexOf =>
+				state.cart = state.cart.filter(producto => producto.id !== currentProuct.id)
+				// si no existe => nunca va pasar
+			}
+		},
+		extraReducers: {
+			[addToCartAsync.pending]: (state, action) => {
+				console.log('is pending', action.payload)
+			},
+			[addToCartAsync.fulfilled]: (state, action) => {
+				console.log('success', action.payload)
 				const currentProduct = action.payload
 				// filter [] y find {}
 				// 
@@ -31,15 +78,16 @@ const storeSlice = createSlice({
 					state.cart.push(currentProduct)
 				}				
 			},
-			removeToCart (state, action) {
-				const currentProuct = action.payload
-				// buscar el producto en el arreglo de cart
-				// si existe => lo eliminamos => creamos un nuevo arreglo sin ese elemento
-				// indexOf =>
-				state.cart = state.cart.filter(producto => producto.id !== currentProuct.id)
-				// si no existe => nunca va pasar
+			[addToCartAsync.rejected]: (state, action) => {
+				console.log('rejected', action.payload)
+				const paylodError = {
+					nombre: "error en la peticion"
+				}
+
+				state.cart.push(paylodError)
+
 			}
-		},
+		}
 })
 
 
@@ -52,9 +100,11 @@ const store = configureStore({
 const useStoreReduxContext = () => {
 	const dispath = useDispatch()
 
+	// si queremos añadir esto que se va guardado en una base de datos
 	const addToCart = (item) => {
     // action = "add_to_cart"
-    dispath(storeSlice.actions.addToCart(item))
+	dispath(addToCartAsync(item))
+    // dispath(storeSlice.actions.addToCart(item))
   }
 
 	const removeToCart = (item) => {
